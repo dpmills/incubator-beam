ad18f102cf6cf4d051eedc3d20d2809b8c7a4612
Daniel Mills <millsd@google.com>
2020-06-09 18:17:17 -0700

    Add an option to disable stateful dofn cleanup timers

--- runners/google-cloud-dataflow-java/worker/src/main/java/org/apache/beam/runners/dataflow/worker/SimpleParDoFn.java
+++ runners/google-cloud-dataflow-java/worker/src/main/java/org/apache/beam/runners/dataflow/worker/SimpleParDoFn.java
@@ -325,7 +325,8 @@ public class SimpleParDoFn<InputT, OutputT> implements ParDoFn {
 
     WindowedValue<InputT> elem = (WindowedValue<InputT>) untypedElem;
 
-    if (fnSignature != null && fnSignature.stateDeclarations().size() > 0) {
+    if (fnSignature != null && fnSignature.stateDeclarations().size() > 0 &&
+        options.as(DataflowPipelineDebugOptions.class).getUseStatefulDoFnCleanupTimers()) {
       registerStateCleanup(
           (WindowingStrategy<?, BoundedWindow>) getDoFnInfo().getWindowingStrategy(),
           (Collection<BoundedWindow>) elem.getWindows());
